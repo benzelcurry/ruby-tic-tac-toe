be69b62683a5a73dc446084beb583fa00b34b375
Ben <benawhiting@gmail.com>
2024-05-17 16:28:41 -0700

    Add GameBoard method for receiving hits

--- classes/game_board.rb
+++ classes/game_board.rb
@@ -9,6 +9,18 @@ class GameBoard
       ]
   end
 
+  # Places a symbol on the square of a player's choice
+  def receive_hit(coordinate, symbol)
+    target = game_board[coordinate[0]][coordinate[1]]
+    if target == ' '
+      self.target = symbol
+    else
+      puts 'This spot is taken. Try again!'
+    end
+
+    # Insert method here for checking if game is over
+  end
+
   private
 
   attr_accessor :game_board
