abecf815feb5658a4a083266c5b7e0884ab7059c
Ben <benawhiting@gmail.com>
2024-05-17 17:16:21 -0700

    Add a method for checking if the game is over

--- classes/game_board.rb
+++ classes/game_board.rb
@@ -9,6 +9,12 @@ class GameBoard
       ]
   end
 
+  # Add a method for printing the game board
+
+  # Add a method for starting a new game (will assign points to players if they win)
+
+  protected
+
   # Places a symbol on the square of a player's choice
   def receive_hit(coordinate, symbol)
     target = game_board[coordinate[0]][coordinate[1]]
@@ -24,4 +30,28 @@ class GameBoard
   private
 
   attr_accessor :game_board
+
+  # Checks to see if the game has been won
+  def game_over?
+    win_found = false
+    row1 = game_board[0]
+    row2 = game_board[1]
+    row3 = game_board[2]
+
+    if game_board.any.all == 'X' || game_board.any.all == 'O'
+      win_found = true
+    elsif row1.include?(symbol)
+      if row1[0] == row2[1] && row1[0] == row3[2]
+        win_found = true
+      elsif row1[2] == row2[1] && row1[2] == row3[0]
+        win_found = true
+      else
+        3.times do |n|
+          win_found = true if row1[n] == row2[n] && row1[n] == row3[n]
+        end
+      end
+    end
+
+    win_found
+  end
 end
