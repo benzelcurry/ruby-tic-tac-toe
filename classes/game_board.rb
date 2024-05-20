9f99a21fe0b7bc176fc93ad8f665c1e0162af411
Ben <benawhiting@gmail.com>
2024-05-20 16:25:51 -0700

    Resolve premature win bug in game_over?

--- classes/game_board.rb
+++ classes/game_board.rb
@@ -25,6 +25,7 @@ class GameBoard
       puts 'This spot is taken. Try again!'
     end
 
+    print_board
     puts "Game over! #{symbol} wins!" if game_over?(symbol)
   end
 
@@ -38,23 +39,20 @@ class GameBoard
   # Todo: Debug first if logic
   def game_over?(symbol)
     win_found = false
-    # row1 = game_board[0]
-    # row2 = game_board[1]
-    # row3 = game_board[2]
+    row1 = game_board[0]
+    row2 = game_board[1]
+    row3 = game_board[2]
 
-    # if game_board.any?.all?(symbol)
-    #   win_found = true
-    # elsif row1.include?(symbol)
-    #   if row1[0] == row2[1] && row1[0] == row3[2]
-    #     win_found = true
-    #   elsif row1[2] == row2[1] && row1[2] == row3[0]
-    #     win_found = true
-    #   else
-    #     3.times do |n|
-    #       win_found = true if row1[n] == row2[n] && row1[n] == row3[n]
-    #     end
-    #   end
-    # end
+    if game_board.any? { |row| row.all?(symbol) }
+      win_found = true
+    elsif row1[0] == symbol && row2[1] == symbol && row3[2] == symbol ||
+          row1[2] == symbol && row2[1] == symbol && row3[0] == symbol
+      win_found = true
+    else
+      3.times do |n|
+        win_found = true if row1[n] == symbol && row2[n] == symbol && row3[n] == symbol
+      end
+    end
 
     win_found
   end
