7e9344695d766e433a6d8ca0041481111462d06a
Ben <benawhiting@gmail.com>
2024-05-21 17:01:12 -0700

    Implement a game loop in the Game class

--- classes/game.rb
+++ classes/game.rb
@@ -1,4 +1,31 @@
 # Class used for instantiating the actual game of tic tac toe
 class Game
-  
+  attr_accessor :game_over, :turn
+
+  def initialize
+    self.game_over = false
+    self.turn = 1
+  end
+
+  def game_loop(player_one, player_two)
+    winner = ''
+
+    until game_over
+      if turn == 1
+        if player_one.place_hit(game_board)
+          self.game_over = true
+          winner = 'Player One'
+        end
+        self.turn = 2
+      else
+        if player_two.place_hit(game_board)
+          self.game_over = true
+          winner = 'Player Two'
+        end
+        self.turn = 1
+      end
+    end
+
+    puts "The game is over! #{winner} has won!"
+  end
 end
--- main.rb
+++ main.rb
@@ -6,17 +6,3 @@ require_relative 'classes/player'
 game_board = GameBoard.new
 player_one = Player.new('Foo', 'X')
 player_two = Player.new('Bar', 'O')
-game_over = false
-turn = 1
-
-until game_over
-  if turn == 1
-    game_over = player_one.place_hit(game_board)
-    turn = 2
-  else
-    game_over = player_two.place_hit(game_board)
-    turn = 1
-  end
-end
-
-puts 'Game over! The winner will be decided after the logic is implemented!'
